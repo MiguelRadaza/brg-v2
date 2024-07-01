b05ef899544684d12b7bfd7e57625683e6709f30
Miguel Igdalino <igdalino.miguel.ph@gmail.com>
2024-07-02 05:24:21 +0800

    Added fix

--- resources/views/verse-list-view.blade.php
+++ resources/views/verse-list-view.blade.php
@@ -2,8 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @laravelPWA
 
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Bible Reading Guide</title>
